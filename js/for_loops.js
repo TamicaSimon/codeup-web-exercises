cb62bee8fea9c6ce19ca7121b00beb91349d508e
Tamica Simon <tamica.simon@gmail.com>
2023-04-03 12:05:18 +0000

    For Loops Updated

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,36 @@
 "use strict"
 
-const number = parseInt(prompt('Enter a number: '));
+// TODO: Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+function showMultiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(num + " x " + i + " = " + num * i);
+    }
+}
 
-for(let i = 1; i <= 10; i++) {
+let num = parseInt(prompt("Enter a number:"));
 
-    const result = i * number;
-    console.log(`${number} * ${i} = ${result}`);
+showMultiplicationTable(num);
+// TODO: Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+
+for (let i = 0; i < 10; i++) {
+    const randomNumber = Math.floor(Math.random() * 181) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + ' is even');
+    } else {
+        console.log(randomNumber + ' is odd');
+    }
+}
+
+//TODO: Create a for loop that uses console.log to create specific output in text.
+for (let i = 1; i <= 9; i++) {
+    let str = '';
+    for (let j = 1; j <= i; j++) {
+        str += i;
+    }
+    console.log(str);
+}
+//TODO: Create a for loop that uses console.log to decrease by 5 from 100 to 0.
+
+for (let i = 100; i >= 5; i -= 5) {
+    console.log(i);
 }
